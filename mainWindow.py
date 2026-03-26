cd58c1260b45e9291634cc5ecbdad94f24ff1df4
xiaoshubao <1379472873@qq.com>
2026-03-26 17:15:46 +0800

    1.增加堆栈窗口，预留兼容更多功能模块;

--- mainWindow.py
+++ mainWindow.py
@@ -6,7 +6,7 @@ from tkinter.filedialog import dialogstates
 import PyQt5
 import PyQt5.QtCore as QtCore
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget, QTextEdit, QMainWindow, QApplication, QAction, \
-    QDesktopWidget, QLabel, QLineEdit, QPushButton, QFileDialog
+    QDesktopWidget, QLabel, QLineEdit, QPushButton, QFileDialog, QStackedWidget, QFormLayout
 import pandas as pd
 import openpyxl
 from openpyxl import load_workbook
@@ -26,16 +26,31 @@ class MainWindow(QMainWindow):
         self.ex = None
         self.set_bar()
 
-        #文本框设定
-        self.path = QtCore.QDir.currentPath()
-        self.line_edit_path = QLineEdit(str(self.path))
-        self.line_edit_path.setFixedHeight(40)
+        #功能模块按键
+        self.btn_mod1 = QPushButton("数据统计")
+        self.btn_mod2 = QPushButton("财务报表")
+        self.btn_mod3 = QPushButton("工资核算")
+        self.btn_mod4 = QPushButton("差旅报销")
 
-        #开始按键
-        self.push_button_start = QPushButton('开始')
-        self.push_button_start.setStyleSheet("background-color: rgb(255,255,255); color: black;")
-        self.push_button_start.setFixedSize(100, 40)
-        self.push_button_start.clicked.connect(self.start_button)
+        self.btn_mod1 = QPushButton("数据统计",self)
+        self.btn_mod2 = QPushButton("财务报表",self)
+        self.btn_mod3 = QPushButton("工资核算",self)
+        self.btn_mod4 = QPushButton("差旅报销", self)
+
+        self.btn_mod1.setFixedSize(80,40)
+        self.btn_mod2.setFixedSize(80,40)
+        self.btn_mod3.setFixedSize(80,40)
+        self.btn_mod4.setFixedSize(80,40)
+
+        self.btn_mod1.setStyleSheet("background-color: rgb(190,226,224); color: black; border-radius: 0px;")
+        self.btn_mod2.setStyleSheet("background-color: rgb(255,255,255); color: black; border-radius: 0px;")
+        self.btn_mod3.setStyleSheet("background-color: rgb(255,255,255); color: black; border-radius: 0px;")
+        self.btn_mod4.setStyleSheet("background-color: rgb(255,255,255); color: black; border-radius: 0px;")
+
+        self.btn_mod1.clicked.connect(self.btn_mod1_clicked)
+        self.btn_mod2.clicked.connect(self.btn_mod2_clicked)
+        self.btn_mod3.clicked.connect(self.btn_mod3_clicked)
+        self.btn_mod4.clicked.connect(self.btn_mod4_clicked)
 
         #放置一个central_widget
         central_widget = QWidget(self)
@@ -46,34 +61,48 @@ class MainWindow(QMainWindow):
         layout_1 = QHBoxLayout()
         layout_2 = QHBoxLayout()
 
-        #实例化按键
-        self.btn = QPushButton("打开需要统计的目录")
-        self.btn.clicked.connect(self.get_dir)
+        #堆栈窗口
+        self.stack1 = QWidget(self)
+        self.stack2 = QWidget(self)
+        self.stack3 = QWidget(self)
+        self.stack4 = QWidget(self)
 
-        #实例化路径输入框并加入布局
-        layout_1.addWidget(self.btn)
-        layout_1.addWidget(self.line_edit_path)
-        layout_1.addWidget(self.push_button_start)
+        #stack1组件
+        self.btn = None
+        self.path = None
+        self.line_edit_path = None
+        self.push_button_start = None
+        self.text_edit = None
+        self.stack1_ui()
 
-        #实例化textEdit并加入布局
-        self.text_edit = QTextEdit()
-        layout_2.addWidget(self.text_edit)
+        # stack2组件
+        self.stack2_ui()
+        # stack3组件
+        self.stack3_ui()
+        # stack4组件
+        self.stack4_ui()
 
-        #实例化plot并加入布局
+        self.Stack = QStackedWidget (self)
+        self.Stack.addWidget (self.stack1)
+        self.Stack.addWidget (self.stack2)
+        self.Stack.addWidget (self.stack3)
+        self.Stack.addWidget (self.stack4)
 
         #设置窗体的宽高和标题
         self.resize(1680,960)
         self.setWindowTitle("ChuChu小助手 V1.0")
 
         #将左侧和右侧布局添加到主水平布局中
+        layout_1.addWidget(self.btn_mod1)
+        layout_1.addWidget(self.btn_mod2)
+        layout_1.addWidget(self.btn_mod3)
+        layout_1.addWidget(self.btn_mod4)
+        layout_2.addWidget(self.Stack)
         main_layout.addLayout(layout_1)
         main_layout.addLayout(layout_2)
         central_widget.setLayout(main_layout)
         #设置窗口的主布局
         self.setCentralWidget(central_widget)
-
-        #调用居中函数
-        self.center()
 
         # 设置信息
         temp_path = QtCore.QDir(self.path)
@@ -98,8 +127,96 @@ class MainWindow(QMainWindow):
                 print("set文件不存在")
                 self.text_edit.append(self.error.format("设置文件不存在"))
 
-            #目标写入文件
-            self.des_sheet = None
+        #目标写入文件
+        self.des_sheet = None
+
+        #调用居中函数
+        self.show()
+        self.center()
+
+    def btn_mod1_clicked(self):
+        self.Stack.setCurrentIndex(0)
+        self.btn_mod1.setStyleSheet("background-color: rgb(190,226,224); color: black; border-radius: 0px;")
+        self.btn_mod2.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod3.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod4.setStyleSheet("background-color: rgb(255,255,255);")
+
+    def btn_mod2_clicked(self):
+        self.Stack.setCurrentIndex(1)
+        self.btn_mod1.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod2.setStyleSheet("background-color: rgb(190,226,224); color: black; border-radius: 0px;")
+        self.btn_mod3.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod4.setStyleSheet("background-color: rgb(255,255,255);")
+
+    def btn_mod3_clicked(self):
+        self.Stack.setCurrentIndex(2)
+        self.btn_mod1.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod2.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod3.setStyleSheet("background-color: rgb(190,226,224); color: black; border-radius: 0px;")
+        self.btn_mod4.setStyleSheet("background-color: rgb(255,255,255);")
+
+    def btn_mod4_clicked(self):
+        self.Stack.setCurrentIndex(3)
+        self.btn_mod1.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod2.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod3.setStyleSheet("background-color: rgb(255,255,255);")
+        self.btn_mod4.setStyleSheet("background-color: rgb(190,226,224); color: black; border-radius: 0px;")
+
+    def stack1_ui(self):
+        print("stack1_ui")
+        #实例化按键
+        self.btn = QPushButton("打开需要统计的目录")
+        self.btn.setStyleSheet("background-color: rgb(255,255,255); color: black;")
+        self.btn.setFixedHeight(40)
+        self.btn.clicked.connect(self.get_dir)
+
+        #文本框设定
+        self.path = QtCore.QDir.currentPath()
+        self.line_edit_path = QLineEdit(str(self.path))
+        self.line_edit_path.setFixedHeight(40)
+
+        #开始按键
+        self.push_button_start = QPushButton('开始')
+        self.push_button_start.setStyleSheet("background-color: rgb(255,255,255); color: black;")
+        self.push_button_start.setFixedSize(100, 40)
+        self.push_button_start.clicked.connect(self.start_button)
+
+        #实例化textEdit并加入布局
+        self.text_edit = QTextEdit()
+
+        stack1_main_layout = QVBoxLayout()
+        stack1_layout_1 = QHBoxLayout()
+        stack1_layout_2 = QHBoxLayout()
+
+        #并加入布局
+        stack1_layout_1.addWidget(self.btn)
+        stack1_layout_1.addWidget(self.line_edit_path)
+        stack1_layout_1.addWidget(self.push_button_start)
+        stack1_layout_2.addWidget(self.text_edit)
+        stack1_main_layout.addLayout(stack1_layout_1)
+        stack1_main_layout.addLayout(stack1_layout_2)
+        self.stack1.setLayout(stack1_main_layout)
+
+    def stack2_ui(self):
+        layout = QFormLayout()
+        layout1 = QHBoxLayout()
+        layout1.addWidget(QLabel("敬请期待1"))
+        layout.addRow(layout1)
+        self.stack2.setLayout(layout)
+
+    def stack3_ui(self):
+        layout = QFormLayout()
+        layout1 = QHBoxLayout()
+        layout1.addWidget(QLabel("敬请期待2"))
+        layout.addRow(layout1)
+        self.stack3.setLayout(layout)
+
+    def stack4_ui(self):
+        layout = QFormLayout()
+        layout1 = QHBoxLayout()
+        layout1.addWidget(QLabel("敬请期待3"))
+        layout.addRow(layout1)
+        self.stack4.setLayout(layout)
 
     def get_dir(self):
         dialog = QFileDialog()
