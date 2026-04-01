f43820abd045e392c32eff5633c57b6d1afa9a40
xiaoshubao <1379472873@qq.com>
2026-04-01 15:23:44 +0800

    1.循环爬取子页信息获取成功;

--- mypackage/src/WebCrawler.py
+++ mypackage/src/WebCrawler.py
@@ -23,11 +23,12 @@ class WebCrawler(QWidget):
         self.valid = '<font color="green">{}</font>'
 
         #请求状态
+        self.request_page_num = 0
         self.request_states = False
 
         #URL设定
         self.label_url = QLabel("输入URL")
-        self.label_url.setStyleSheet("background-color: rgb(255,255,255); color: black;")
+        self.label_url.setStyleSheet("background-color: rgb(225,225,225); color: black;")
         self.label_url.setFixedHeight(40)
         self.line_edit_path = QLineEdit(str('https://gs.amac.org.cn/'))
         self.line_edit_path.setFixedHeight(40)
@@ -35,7 +36,7 @@ class WebCrawler(QWidget):
 
         #关键词设定
         self.label_keyword = QLabel("输入搜索关键词")
-        self.label_keyword.setStyleSheet("background-color: rgb(255,255,255); color: black;")
+        self.label_keyword.setStyleSheet("background-color: rgb(225,225,225); color: black;")
         self.label_keyword.setFixedHeight(40)
         self.search_keyword = QLineEdit('进化论')
         self.search_keyword.setFixedSize(120, 40)
@@ -71,22 +72,16 @@ class WebCrawler(QWidget):
         self.timer = None
 
     def request_fun(self):
-        # headers = {
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
-        # }
-        # web = self.line_edit_path.text()
-        # self.timer = threading.Timer(3, self.repeat_fun, args=(headers, web))
-        # self.timer.start()
-
-        self.timer = threading.Timer(3, self.js_load_fun)
+        self.timer = threading.Timer(1, self.fund_search, args=str(self.request_page_num))
         self.timer.start()
 
 
-    def js_load_fun(self):
-        #目标网站
-        api_url = str(self.line_edit_path.text()) + '/amac-infodisc/api/pof/fund?&page=0&size=20'
+    def fund_search(self, page):
+        #目标网站,page默认从0开始，size最大值是100
+        page_num = str(page)
+        api_url = str(self.line_edit_path.text()) + f'/amac-infodisc/api/pof/fund?&page={page_num}&size=100'
+        print("url:", api_url)
 
-        print(api_url)
         payload = {
             'keyword': str(self.search_keyword.text()),
         }
@@ -107,7 +102,7 @@ class WebCrawler(QWidget):
         try:
             response.raise_for_status()  # 如果状态码不是 200，会抛出异常
             print("请求成功！")
-            self.text_edit.append(self.valid.format("请求成功"))
+            self.text_edit.append(self.valid.format("请求成功:"+api_url))
             
             # 6. 解析返回的数据 (通常是 JSON 格式)
             data = response.json()
@@ -115,24 +110,36 @@ class WebCrawler(QWidget):
             # 7. 将数据转换为 DataFrame (以 pandas 处理为例)
             # 你需要根据实际的 JSON 结构调整代码
             # 假设数据在 data['results'] 这个键下面
-            print(data)  # 打印完整数据以便分析
+            # print(data)  # 打印完整数据以便分析
             if 'content' in data:
                 df = pd.DataFrame(data['content'])
                 if df.empty:
                     print("没有搜索到相关记录")
                     self.text_edit.append(self.error.format("没有搜索到相关记录"))
                 else:
+                    request_page_max = int(data['totalElements'])//100      #//是整除
+                    records_message = "找到" + str(data['totalElements']) + "条记录"
+                    print(records_message)
+
                     # df.drop(['id', 'managerType', 'lastQuarterUpdate'], axis=1, inplace=True)
                     print(df[['fundNo', 'fundName', 'managerName', 'establishDate', 'putOnRecordDate']])
 
+                    # 判断是否需要重复执行
+                    print("page:", str(self.request_page_num), str(request_page_max))
+                    if self.request_page_num < request_page_max:
+                        self.request_page_num += 1
+                        self.request_fun()
+                    else:
+                        self.text_edit.append(self.valid.format(records_message))
             else:
                 print("未找到预期的数据键，请检查 JSON 结构。")
                 print(data)  # 打印完整数据以便分析
 
         except requests.exceptions.HTTPError as e:
             print(f"请求失败: {e}")
+            self.stack_close()
 
-    def repeat_fun(self, headers, url):
+    def demo_search(self, headers, url):
         response = requests.get(url, headers=headers)
 
         response.encoding = "utf-8" # 根据网页实际编码设置
@@ -269,6 +276,7 @@ class WebCrawler(QWidget):
             self.push_button_start.setText('请求')
             self.timer.cancel()
         else:
+            self.request_page_num = 0
             self.request_states = True
             print('请求开始')
             self.push_button_start.setText('停止')
